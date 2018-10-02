5def92c3f2f860a34f02ae9b08aa87efa1a9e2ca
yuchank <no1915@gmail.com>
2018-10-02 16:36:32 +0900

    attack

--- app.js
+++ app.js
@@ -9,6 +9,15 @@ var usersRouter = require('./app_server/routes/users');
 
 var app = express();
 
+// socket.io
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+
+var util = require('util');
+
+// node app.js
+server.listen(8000);
+
 // view engine setup
 app.set('views', path.join(__dirname, 'app_server', 'views'));
 app.set('view engine', 'pug');
@@ -39,3 +48,68 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
+var clients = [];
+
+// connection event handler
+io.sockets.on('connection', function(socket) {
+
+  clients.push(socket.id);
+  var clientConnectedMsg = `User connected ${util.inspect(socket.id)}, total: ${clients.length} `;
+  console.log(clientConnectedMsg);
+
+  // // login event 
+  // socket.on('login', function(data) {
+  //   console.log(`Client logged-in:\n name: ${data.name} \n userid: ${data.userid}` );
+
+  //   // socket에 client 정보를 저장
+  //   socket.name = data.name;
+  //   socket.userid = data.userid;
+
+  //   // 접속된 모든 client에게 message를 전송
+  //   io.sockets.emit('login', data.name);
+  // });
+
+  // // chat event
+  // socket.on('chat', function (data) {
+  //   console.log(`Message from ${socket.name}: ${data.msg}`);
+  //   var msg = {
+  //     from: {
+  //       name: socket.name,
+  //       userid: socket.userid      
+  //     },
+  //     msg: data.msg
+  //   };
+
+  //   // message를 전송한 client를 제외한 모든 client에게 message를 전송
+  //   socket.broadcast.emit('chat', msg);
+
+  //   // message를 전송한 client에게만 message를 전송
+  //   // socket.emit('example message 1', { hello: 'world 1' });
+  
+  //   // 특정 client에게만 message를 전송. id는 socket 객체의 id 속성값이다. 
+  //   // io.to(socket.id).emit('event_name', {});
+  // });
+
+  // // force client disconnect from server
+  // socket.on('forceDisconnect', function() {
+  //   socket.disconnect();
+  // });
+
+  socket.on('disconnect', function() {
+    clients.pop(socket.id);
+    var clientDisconnectedMsg = `User disconnected ${util.inspect(socket.id)}, total: ${clients.length}`;
+    console.log(clientDisconnectedMsg);
+  });
+});
+
+function getRandomInRange(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+function sendAttack() {
+  console.log('Attack sent to user');
+  io.sockets.emit('attack', getRandomInRange(0, 360));
+}
+
+setInterval(sendAttack, 3000);
